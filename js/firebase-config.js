29c0faf8e627e61ecd0ac09f409fe04e32463259
wonyoung <144025008+CarlosJoseAlbiol@users.noreply.github.com>
2026-08-18 12:48:10 +0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcjNiuKxpUE6h-CShTWVdY5RZuYZqHy6k",
+  authDomain: "mseufcs-assessment.firebaseapp.com",
+  projectId: "mseufcs-assessment",
+  storageBucket: "mseufcs-assessment.firebasestorage.app",
+  messagingSenderId: "141625437207",
+  appId: "1:141625437207:web:daf33f36e8c40cc3148052"
+};
